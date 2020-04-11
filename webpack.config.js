7e43fee566d560ce235a612c713176233d2ccfdf
Radiophysiker <bolshakovdmitry@bk.ru>
2020-04-11 12:52:40 +0300

    add react config

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,33 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const config = {
-  entry: ["./src/index.ts"],
+  entry: ["./src/index.tsx"],
+  devtool: "source-map",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "main.js",
-    publicPath: "dist/",
+    publicPath: "./",
+  },
+  devServer: {
+    historyApiFallback: true,
   },
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
   },
   module: {
     rules: [
       {
-        test: [/\.js?$/, /\.ts?$/],
+        test: /\.(js|ts)x?$/,
         loader: "babel-loader",
         exclude: "/node_modules/",
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
 };
 module.exports = config;
